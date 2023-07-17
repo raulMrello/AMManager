d717c77f00b3342b87ef7cb54ffd8dc17404cf35
raulMrello <raul.martin.rello@gmail.com>
2023-07-17 13:42:24 +0200

    bugfix para obtener el factor de potencia a través de p/v*i

--- AMManager.cpp
+++ AMManager.cpp
@@ -887,6 +887,16 @@ void AMManager::_measure(bool enable_notif) {
 							_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.aPow = _amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.voltage * _amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.current * _amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.pfactor;
 							if(dobj->drv->getModel() == VERS_METERING_AM_CTX3_MODEL_P1){
 								_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.aPow = amr->params.aPow;
+								double vi = amr->params.voltage * amr->params.current;
+								if(vi > 0){
+									_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.pfactor = amr->params.aPow/vi;
+								}
+								else{
+									_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.pfactor = 1.0;
+								}
+								_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.pfactor = (_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.pfactor > 1.0) ? 1.0 : _amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.pfactor;
+								_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.flags |= MeteringAnalyzerPowerFactor;
+								DEBUG_TRACE_D(_EXPR_, _MODULE_, "Analizador=[%d], pfactor=%.02f", (base_analyzer + amr->analyzer),_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.pfactor);
 							}
 							_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.flags |= MeteringAnalyzerActivePower;
 							DEBUG_TRACE_D(_EXPR_, _MODULE_, "Analizador=[%d], aPow=%.02fW amr->params.aPow=%.02fW", (base_analyzer + amr->analyzer),_amdata.analyzers[(base_analyzer + amr->analyzer)].stat.measureValues.aPow, amr->params.aPow);
